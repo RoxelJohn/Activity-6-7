7047f259ba6427deff8ffbcc224a910b230b1984
Ace <acebermoy@gmail.com>
2024-02-12 19:11:14 +0800

    Initial Commit

--- src/main/kotlin/Activity1.kt
+++ src/main/kotlin/Activity1.kt
@@ -1,2 +1,18 @@
 package org.example
 
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    // Creates a reader instance which takes
+    // input from standard input - keyboard
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    // nextInt() reads the next integer from the keyboard
+    var integer:Int = reader.nextInt()
+
+    // println() prints the following line to the output screen
+    println("You entered: $integer")
+}
+
