4cb23e622c99131869a06d3e74571725fcddebef
Ace <acebermoy@gmail.com>
2024-02-12 19:16:40 +0800

    Initial Commit

--- src/main/kotlin/Activity1.kt
+++ src/main/kotlin/Activity1.kt
@@ -7,12 +7,17 @@ fun main(args: Array<String>) {
     // Creates a reader instance which takes
     // input from standard input - keyboard
     val reader = Scanner(System.`in`)
-    print("Enter a number: ")
+    print("Enter first number: ")
 
     // nextInt() reads the next integer from the keyboard
-    var integer:Int = reader.nextInt()
+    val first:Int = reader.nextInt()
+
+    print("Enter second number: ")
+    val second:Int = reader.nextInt()
+
+    var sum = first + second
 
     // println() prints the following line to the output screen
-    println("You entered: $integer")
+    println("You entered: $sum")
 }
 
