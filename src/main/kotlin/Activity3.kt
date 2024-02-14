4c65fa3daf531d7c10504f67868a42375d316ad0
Ace <acebermoy@gmail.com>
2024-02-14 16:41:21 +0800

    Activity2

--- src/main/kotlin/Activity3.kt
+++ src/main/kotlin/Activity3.kt
@@ -13,7 +13,7 @@ fun main() {
 fun getBirthYear():Int{
     println("Enter your birth year: ")
     val input = readLine()?.toIntOrNull()
-    return if(input == null||input <0){
+    return if(input == null||input <0|| input >2024){
         println("Invalid input. Please enter a valid year.")
         getBirthYear()
     }else{
@@ -27,6 +27,7 @@ fun calculateAge(birthYear: Int): Int
     }
 fun determineAgeGroup(age:Int):String{
     return when{
+        age in 0..1->"Baby"
         age in 1..17 ->"Underage"
         age in 18..30->"Young Adult"
         age in 31..59->"Adult"
