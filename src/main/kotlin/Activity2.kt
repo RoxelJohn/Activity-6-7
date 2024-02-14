f5deec65cdb8080de6974bb09d9273e531c563b4
Ace <acebermoy@gmail.com>
2024-02-14 17:19:25 +0800

    Activity2

--- src/main/kotlin/Activity2.kt
+++ src/main/kotlin/Activity2.kt
@@ -1,2 +1,26 @@
 package org.example
 
+fun main(){
+    val noOfStud = 5
+    val subjects = listOf("Math", "English", "Filipino", "Science")
+    for (i in 1..noOfStud) {
+        println("Write your name: ")
+        val name = readLine()
+        var totalGrade = 0.0
+
+        println("The grades for $name:")
+        subjects.forEach { subject ->
+            totalGrade += getGrade(subject)
+        }
+        val averageGrade = totalGrade / subjects.size
+        println("$name, your average grade is : $averageGrade")
+    }
+}
+
+fun getGrade(subject: String): Double {
+    println("Enter your grade in $subject: ")
+    return readLine()?.toDoubleOrNull() ?: run {
+        println("Invalid input. Please enter a valid grade.")
+        getGrade(subject)
+    }
+}
